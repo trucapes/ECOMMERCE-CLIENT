05ba343afcf9d5adb0996bce7db3d27380cc046a
Aditya Ranjan <arsrivastawa2710@gmail.com>
2024-01-30 14:07:47 +0530

    NavBar brand logo updated

--- src/components/Nav/Nav-Brand/Navbrand.js
+++ src/components/Nav/Nav-Brand/Navbrand.js
@@ -1,14 +1,15 @@
-import './NavBrand.css'
-import { Link } from 'react-router-dom';
+import "./NavBrand.css";
+import { Link } from "react-router-dom";
+import Logo from "../../../asset/brand/logo.png";
 
 const NavBrand = () => {
-    return ( 
-        <div href="#home" className='navbrand__container'>
-           <h1 className='navbrand'>
-               <Link to="/">SHEMA</Link>
-            </h1>
-        </div>
-     );
-}
- 
+  return (
+    <div href="#home" className="navbrand__container">
+      <Link to={"/"} className="navbrand__logo__container">
+        <img className="navbrand__logo" src={Logo} alt="" />
+      </Link>
+    </div>
+  );
+};
+
 export default NavBrand;
